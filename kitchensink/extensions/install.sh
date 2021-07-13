fed5865fc2d78278ad5c3c3805a2e9e6659d65b8
Ryan Voon <voonck@gmail.com>
2021-07-13 20:51:46 +0800

    Update install.sh

--- kitchensink/extensions/install.sh
+++ kitchensink/extensions/install.sh
@@ -8,4 +8,4 @@ echo "/system-property=YTLC.Environment.Properties:add(value=/opt/eap/portal/)" 
 echo "/subsystem=logging/periodic-rotating-file-handler=FILE:add(autoflush=true,append=true,named-formatter=COLOR-PATTERN,file={path=server.log,relative-to=jboss.server.log.dir},level=INFO,suffix=.yyyy-MM-dd)" >> ${cli_script}
 echo "/subsystem=logging/root-logger=ROOT:add-handler(name=FILE)" >> ${cli_script}
 
-run_cli_script ${cli_script}
+exec_cli_script ${cli_script}
